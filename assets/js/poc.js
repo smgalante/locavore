7a6b9f26552d5810ec2436bd5c37aa95afdfcf82
Arnold Lei <arnold.lei88@gmail.com>
2016-06-17 16:13:05 -0400

    successfully linked the farmers market api to the google maps api

--- assets/js/poc.js
+++ assets/js/poc.js
@@ -1,10 +1,9 @@
-(function(window, google, mapster){
+(function(window, google, mapster, $) {
 
     var marketId = []; //returned from the API
     var allLatlng = []; //returned from the API
     var allMarkers = []; //returned from the API
     var marketName = []; //returned from the API
-    var infowindow = null;
     var pos;
     var userCords;
     var tempMarkerHolder = [];
@@ -13,49 +12,90 @@
     var options = mapster.MAP_DEFAULT_OPTIONS
 
     var map = new google.maps.Map(element, options);
-    var location = new google.maps.LatLng(40, -74);
+    var location = new google.maps.LatLng(40.4794355, -74.4196301);
     var service = new google.maps.places.PlacesService(map);
+
     var request = {
-      location: location,
-      radius: '5000',
-      types: ['bar']
+        location: location,
+        radius: '5000',
+        types: ['farmers market']
     };
 
     var infowindow = new google.maps.InfoWindow();
+   //  var input = document.getElementById('pac-input');
+   //  var autocomplete = new google.maps.places.Autocomplete(input);
+  	// autocomplete.bindTo('bounds', map);
 
+
+    $('#search').submit(function() {
+        var search = $('#userSearch').val();
+        var url = "http://search.ams.usda.gov/farmersmarkets/v1/data.svc/zipSearch?zip=" + search;
+        $.ajax({
+            type: "GET",
+            contentType: "application/json; charset=utf-8",
+            url: url,
+            dataType: 'jsonp',
+            success: function(data) {
+                var i = 0;
+                for (; i < data.results.length; i++) {
+                    marketId.push(data.results[i].id);
+                    marketName.push(data.results[i].marketname);
+                }
+            }
+        }).done(function(f){
+        	var i=0;
+        	for (; i<marketId.length; i++){
+        		$.ajax({
+        			type: 'GET',
+        			contentType: 'application/json; charset=utf-8',
+        			url: 'http://search.ams.usda.gov/farmersmarkets/v1/data.svc/mktDetail?id=' + marketId[i],
+        			dataType: 'jsonp',
+        			success: function(data){
+        				for(var k in data){
+        					var results = data[k].GoogleLink;
+        					var latLong = decodeURIComponent(results.substring(results.indexOf("=")+1, results.lastIndexOf("(")));
+        					var split = latLong.split(',');
+        					var latitude = parseFloat(split[0]);
+							var longitude = parseFloat(split[1]);
+							myLatlng = new google.maps.LatLng(latitude,longitude);
+							allMarkers = new google.maps.Marker({
+								position: myLatlng,
+								map: map,
+								title: marketName[i],
+							})
+        				}
+        			},
+        			dataType: 'jsonp',
+        		});
+        	}
+        });
+
+        return false;
+    });
 
     function createMarker(place) {
-      console.log(place);
-      var placeLoc = place.geometry.location;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-      });
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+        });
 
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-        $('#picture').html(place.image)
-      });
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+            $('#picture').html(place.image)
+        });
     }
 
     service.nearbySearch(request, function(results, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          createMarker(results[i]);
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+                console.log(results[i])
+            }
         }
-      }
     });
 
 
-    $('#search').submit(function(){
-      var search = $('#userSearch').val();
-      console.log(search);
 
-      return false;
-    })
-
-
-
-
-}(window, google, window.Mapster));
+}(window, google, window.Mapster, window.$));
