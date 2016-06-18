2db6b9583d43c5372313dc1eb52abed43ca45f68
Steven Galante <smgalante85@gmail.com>
2016-06-18 12:31:41 -0400

    box update

--- assets/js/poc.js
+++ assets/js/poc.js
@@ -6,13 +6,12 @@
     var marketName = []; //returned from the API
     var pos;
     var userCords;
-    var tempMarkerHolder = [];
 
     var element = document.getElementById('map');
     var options = mapster.MAP_DEFAULT_OPTIONS
 
     var map = new google.maps.Map(element, options);
-    var location = new google.maps.LatLng(40.4794355, -74.4196301);
+    // var location = new google.maps.LatLng(40.4794355, -74.4196301);
     var service = new google.maps.places.PlacesService(map);
 
     var request = {
@@ -22,9 +21,9 @@
     };
 
     var infowindow = new google.maps.InfoWindow();
-   //  var input = document.getElementById('pac-input');
-   //  var autocomplete = new google.maps.places.Autocomplete(input);
-  	// autocomplete.bindTo('bounds', map);
+    var input = document.getElementById('pac-input');
+    var autocomplete = new google.maps.places.Autocomplete(input);
+  	autocomplete.bindTo('bounds', map);
 
 
     $('#search').submit(function() {
@@ -43,6 +42,7 @@
                 }
             }
         }).done(function(f){
+        	var x=0;
         	var i=0;
         	for (; i<marketId.length; i++){
         		$.ajax({
@@ -53,48 +53,57 @@
         			success: function(data){
         				for(var k in data){
         					var results = data[k].GoogleLink;
+        					var address = data[k].Address;
         					var latLong = decodeURIComponent(results.substring(results.indexOf("=")+1, results.lastIndexOf("(")));
         					var split = latLong.split(',');
         					var latitude = parseFloat(split[0]);
 							var longitude = parseFloat(split[1]);
 							myLatlng = new google.maps.LatLng(latitude,longitude);
+							console.log(data[k]);
 							allMarkers = new google.maps.Marker({
 								position: myLatlng,
 								map: map,
-								title: marketName[i],
-							})
-        				}
+								title: marketName[x],
+							});	
+							//Where all the onclick should go for the modals
+							google.maps.event.addListener(allMarkers, 'click', function(){
+								infowindow.setContent(address);
+								infowindow.open(map, this);
+							}) 
+							console.log(i);	
+							x++;        				}
         			},
         			dataType: 'jsonp',
-        		});
+        		})	
         	}
+        }).done(function(f){
+        	console.log(f);
         });
-
         return false;
     });
 
-    function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-        });
+    // function createMarker(place) {
+    //     var placeLoc = place.geometry.location;
+    //     var marker = new google.maps.Marker({
+    //         map: map,
+    //         position: place.geometry.location
+    //     });
 
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
-            $('#picture').html(place.image)
-        });
-    }
+    //     google.maps.event.addListener(marker, 'click', function() {
+    //         infowindow.setContent(place.name);
+    //         infowindow.open(map, this);
+    //         $('#picture').html(place.image)
+    //     });
+    // }
 
-    service.nearbySearch(request, function(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-                console.log(results[i])
-            }
-        }
-    });
+    // service.nearbySearch(request, function(results, status) {
+    //     if (status == google.maps.places.PlacesServiceStatus.OK) {
+    //         for (var i = 0; i < results.length; i++) {
+    //             createMarker(results[i]);
+    //             console.log(results[i])
+    //         }
+    //     }
+    // });
 
 
 
