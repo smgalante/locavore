860fb76a2da6f96da69e704cae96e6cd8bd6789d
Steven Galante <smgalante85@gmail.com>
2016-06-18 12:07:09 -0400

    small change

--- assets/js/box.js
+++ assets/js/box.js
@@ -1,8 +1,8 @@
  $('#test').click(function(){
-    bootbox.dialog({ 
+    bootbox.dialog({ //custom modal dialog box
     	message:'You successfully click this button',
     	title: 'Highland Park, NJ',
     	onEscape: function(){},
-    	
+
   });
 });
