5a12a0e119d62c742d416c31aba75af216e82312
Steven Galante <smgalante85@gmail.com>
2016-06-18 11:49:11 -0400

    initial modal code added

---
+++ assets/js/box.js
@@ -0,0 +1,8 @@
+ $('#test').click(function(){
+    bootbox.dialog({ 
+    	message:'You successfully click this button',
+    	title: 'Highland Park, NJ',
+    	onEscape: function(){},
+    	
+  });
+});
