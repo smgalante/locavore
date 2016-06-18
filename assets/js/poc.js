74aaf0ab8309cdf0e59eedf9c8dc3e8954e40d9d
Arnold Lei <arnold.lei88@gmail.com>
2016-06-18 12:28:00 -0400

    Finished comments

--- assets/js/poc.js
+++ assets/js/poc.js
@@ -78,40 +78,14 @@
 									infowindow.setContent(address);
 									infowindow.open(map, this);
 							}) 
-							console.log(i);	
 							x++;        				}
         			},
         			dataType: 'jsonp',
         		})	
         	}
-        }).done(function(f){
-        	console.log(f);
         });
         return false;
     });
-
-    // function createMarker(place) {
-    //     var placeLoc = place.geometry.location;
-    //     var marker = new google.maps.Marker({
-    //         map: map,
-    //         position: place.geometry.location
-    //     });
-
-    //     google.maps.event.addListener(marker, 'click', function() {
-    //         infowindow.setContent(place.name);
-    //         infowindow.open(map, this);
-    //         $('#picture').html(place.image)
-    //     });
-    // }
-
-    // service.nearbySearch(request, function(results, status) {
-    //     if (status == google.maps.places.PlacesServiceStatus.OK) {
-    //         for (var i = 0; i < results.length; i++) {
-    //             createMarker(results[i]);
-    //             console.log(results[i])
-    //         }
-    //     }
-    // });
 
 
 
