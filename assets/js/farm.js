5902d3962da85fd8ef4fc49a4a5e8dd26e80d8d5
Arnold Lei <arnold.lei88@gmail.com>
2016-06-20 17:07:10 -0400

    Working on using geocoding to automatically generate a zip code

--- assets/js/farm.js
+++ assets/js/farm.js
@@ -1,4 +1,18 @@
 (function(window, google, $){
-  
+	var Farm = (function(){
+    function Farm(){
+
+    }
+    Mapster.prototype = {
+      
+    }
+    return Mapster;
+  }());
+
+  Farm.init = function(){
+
+  };
+
+  window.Farm = Farm;
   
 }(window, google, window.$))
